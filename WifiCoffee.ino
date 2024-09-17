35942b907138a4654a7423a695cace4f40594836
Salvo <s.contino@gmail.com>
2024-09-17 22:53:10 +0200

    Refactor code to use ArduinoWebsockets for WebSocket connection

--- WifiCoffee.ino
+++ WifiCoffee.ino
@@ -1,5 +1,5 @@
 #include <WiFi.h>
-#include <WebSocketsClient.h>
+#include <ArduinoWebsockets.h>
 #include <HTTPClient.h>
 
 const char* ssid = "SSID";
@@ -9,7 +9,7 @@ bool accendi = false;
 bool manopola = false;
 int secondi_aspettare = 20;  
 
-WebSocketsClient webSocket;
+websockets::WebsocketsClient webSocket;
 HTTPClient http;
 const char* serverAddress = "192.168.1.137"; 
 const int serverPort = 8080;   
@@ -17,43 +17,39 @@ const int serverPort = 8080;
 const char* COFFEE_MACHINE_ID = "COFFEE_MACHINE_ID";
 const char* COFFEE_MACHINE_TOKEN = "COFFEE_MACHINE_TOKEN";
 
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+void onMessageCallback(websockets::WebsocketsMessage message) {
+    Serial.printf("Ricevuto messaggio: %s\n", message.data().c_str());
+    if (message.data() == "Accendi" || message.data() == "Spegni") {
+        accendi = true;
+        Serial.println("Accensione eseguita");
+    } else if (message.data() == "Manopola") {
+        manopola = true;
+        Serial.println("Manopola girata");
+    } else if (message.data().startsWith("SetSecondi")) {
+        int new_seconds;
+        sscanf(message.data().c_str(), "SetSecondi %d", &new_seconds);
+        secondi_aspettare = new_seconds;
+        Serial.printf("Secondi da aspettare impostati a: %d\n", secondi_aspettare);
+    }
+}
 
-    switch(type) {
-        case WStype_DISCONNECTED:
-            Serial.println("Disconnesso dal WebSocket server");
-            // Try to reconnect to the WebSocket server
-            while (!webSocket.isConnected()) {
-              Serial.println("Tentativo di riconnessione al WebSocket server...");
-              webSocket.begin(serverAddress, serverPort, "/");
-              webSocket.loop();
-              delay(5000); // Wait 5 seconds before trying again
-            }
-            Serial.println("Riconnesso al WebSocket server");
-            break;
-        case WStype_CONNECTED:
-            Serial.println("Connesso al WebSocket server");
-            webSocket.sendTXT("{ ID: \"" + String(COFFEE_MACHINE_ID) + "\", TOKEN: \"" + String(COFFEE_MACHINE_TOKEN) + "\" }");
-            break;
-        case WStype_TEXT:
-            Serial.printf("Ricevuto messaggio: %s\n", payload);
-            if (strcmp((const char*)payload, "Accendi") == 0 || strcmp((const char*)payload, "Spegni") == 0) {
-                accendi = true;
-                Serial.println("Accensione eseguita");
-            } else if (strcmp((const char*)payload, "Manopola") == 0) {\
-                manopola = true;
-                Serial.println("Manopola girata");
-            } else if (strncmp((const char*)payload, "SetSecondi", 10) == 0) {
-                int new_seconds;
-                sscanf((const char*)payload, "SetSecondi %d", &new_seconds);
-                secondi_aspettare = new_seconds;
-                Serial.printf("Secondi da aspettare impostati a: %d\n", secondi_aspettare);
-            }
-            break;
-        case WStype_BIN:
-            Serial.println("Ricevuto messaggio binario");
-            webSocket.sendTXT("Messaggio binario ricevuto");
-            break;
+void onEventsCallback(websockets::WebsocketsEvent event, String data) {
+    if (event == websockets::WebsocketsEvent::ConnectionOpened) {
+        Serial.println("Connesso al WebSocket server");
+        webSocket.send("{ ID: \"" + String(COFFEE_MACHINE_ID) + "\", TOKEN: \"" + String(COFFEE_MACHINE_TOKEN) + "\" }");
+    } else if (event == websockets::WebsocketsEvent::ConnectionClosed) {
+        Serial.println("Disconnesso dal WebSocket server");
+        // Try to reconnect to the WebSocket server
+        while (!webSocket.available()) {
+            Serial.println("Tentativo di riconnessione al WebSocket server...");
+            webSocket.connect(serverAddress, serverPort, "/");
+            delay(5000); // Wait 5 seconds before trying again
+        }
+        Serial.println("Riconnesso al WebSocket server");
+    } else if (event == websockets::WebsocketsEvent::GotPing) {
+        Serial.println("Ping ricevuto");
+    } else if (event == websockets::WebsocketsEvent::GotPong) {
+        Serial.println("Pong ricevuto");
     }
 }
 
@@ -87,20 +83,20 @@ void setup() {
   http.end();
 
   // Configura WebSocket
-  webSocket.begin(serverAddress, serverPort, "/");
-  webSocket.onEvent(webSocketEvent);
+  webSocket.onMessage(onMessageCallback);
+  webSocket.onEvent(onEventsCallback);
+  webSocket.connect(serverAddress, serverPort, "/");
 
   // Try to connect to the WebSocket server
-  while (!webSocket.isConnected()) {
+  while (!webSocket.available()) {
     Serial.println("Tentativo di connessione al WebSocket server...");
-    webSocket.loop();
     delay(5000); // Wait 5 seconds before trying again
   }
   Serial.println("Connesso al WebSocket server");
 }
 
 void loop() {
-  webSocket.loop();
+  webSocket.poll();
   
   if(accendi){
     digitalWrite(33, HIGH);
@@ -121,4 +117,3 @@ void loop() {
     manopola = false;
   }
 }
-
